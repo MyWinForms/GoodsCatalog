5c5a13da0b4a0dc3bcc0be983ecbfd10512c1053
JIeHka <pogorelova_elena_1989@ukr,net>
2021-09-14 17:20:03 +0300

    Добавлены CRUD-операции

--- GoodsCatalog/CategoriesEditor.Designer.cs
+++ GoodsCatalog/CategoriesEditor.Designer.cs
@@ -29,12 +29,73 @@ namespace GoodsCatalog
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.title = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameField = new System.Windows.Forms.TextBox();
+            this.executeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.title.Location = new System.Drawing.Point(54, 28);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(203, 18);
+            this.title.TabIndex = 0;
+            this.title.Text = "Операции с категориями";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Наименование категории:\r\n";
+            // 
+            // nameField
+            // 
+            this.nameField.Location = new System.Drawing.Point(32, 90);
+            this.nameField.Name = "nameField";
+            this.nameField.Size = new System.Drawing.Size(245, 21);
+            this.nameField.TabIndex = 2;
+            // 
+            // executeButton
+            // 
+            this.executeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.executeButton.Location = new System.Drawing.Point(107, 129);
+            this.executeButton.Name = "executeButton";
+            this.executeButton.Size = new System.Drawing.Size(92, 27);
+            this.executeButton.TabIndex = 3;
+            this.executeButton.Text = "Выполнить";
+            this.executeButton.UseVisualStyleBackColor = true;
+            this.executeButton.Click += new System.EventHandler(this.executeButton_Click);
+            // 
+            // CategoriesEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(311, 186);
+            this.Controls.Add(this.executeButton);
+            this.Controls.Add(this.nameField);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.title);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Name = "CategoriesEditor";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "CategoriesEditor";
+            this.Load += new System.EventHandler(this.CategoriesEditor_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nameField;
+        private System.Windows.Forms.Button executeButton;
     }
 }
--- GoodsCatalog/LoginWindow.Designer.cs
+++ GoodsCatalog/LoginWindow.Designer.cs
@@ -29,12 +29,84 @@ namespace GoodsCatalog
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.loginField = new System.Windows.Forms.TextBox();
+            this.passwField = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(39, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Логин:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(27, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Пароль:";
+            // 
+            // loginButton
+            // 
+            this.loginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loginButton.Location = new System.Drawing.Point(116, 122);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(83, 31);
+            this.loginButton.TabIndex = 2;
+            this.loginButton.Text = "Войти";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // loginField
+            // 
+            this.loginField.Location = new System.Drawing.Point(106, 35);
+            this.loginField.Name = "loginField";
+            this.loginField.Size = new System.Drawing.Size(183, 21);
+            this.loginField.TabIndex = 3;
+            // 
+            // passwField
+            // 
+            this.passwField.Location = new System.Drawing.Point(106, 76);
+            this.passwField.Name = "passwField";
+            this.passwField.PasswordChar = '*';
+            this.passwField.Size = new System.Drawing.Size(183, 21);
+            this.passwField.TabIndex = 4;
+            // 
+            // LoginWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(316, 170);
+            this.Controls.Add(this.passwField);
+            this.Controls.Add(this.loginField);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Name = "LoginWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "LoginWindow";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.TextBox loginField;
+        private System.Windows.Forms.TextBox passwField;
     }
 }
